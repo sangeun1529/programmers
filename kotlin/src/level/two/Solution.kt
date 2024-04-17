bf162818b49e214a483bbaafc9c637654b8882f9
kkomac <sangeun.kim@rnrvh.com>
2024-04-17 10:38:56 +0900

    solution: level Two targetNumber.

--- kotlin/src/level/two/Solution.kt
+++ kotlin/src/level/two/Solution.kt
@@ -10,7 +10,7 @@ class Solution
 /**
  * https://school.programmers.co.kr/learn/courses/30/lessons/70129
  */
-class Solution0002 {
+class Solution0001 {
     fun solution(s: String): List<Int> {
         var convertCount = 0
         var removeZero = 0
@@ -29,3 +29,26 @@ class Solution0002 {
 
 }
 
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/43165
+ */
+class Solution0002 {
+
+    fun solution(numbers: IntArray, target: Int): Int {
+        var answer = 0
+
+        fun dfs(index: Int, result: Int) {
+            if (index == numbers.size) {
+                if (result == target) {
+                    answer += 1
+                }
+            } else {
+                dfs(index + 1, result + numbers[index])
+                dfs(index + 1, result - numbers[index])
+            }
+        }
+
+        dfs(0, 0)
+        return answer
+    }
+}
